e11f8adf5b14e4f3022818fe10ed66fa2ed3f6f5
Noah Sauber <nds5yf@virginia.edu>
2016-05-23 15:14:08 -0400

    projector

---
+++ projector.py
@@ -0,0 +1,38 @@
+#Michael Eller
+#Noah Sauber
+#23 July 2015
+
+import visa
+import numpy
+import time as time
+import win32com.client
+
+class PPT(object):
+
+	def __init__(self, name, kind = "hadamard", rank = 1):
+		accepted_kinds = {'hadamard', 'bar', 'raster'}
+
+		if kind in accepted_kinds:
+			self.name = name
+			self.kind = kind
+			self.rank = rank
+			self.Application = win32com.client.Dispatch("PowerPoint.Application")
+			self.Presentation = self.Application.Presentations.Open(FileName= str(self.name))
+			self.SS = self.Application.SlideShowWindows(1)
+			print "NOTE: Projector has been initialized."
+
+		else:
+			print "ERROR: Invalid projector kind"
+
+	def run(self):
+	    for x in range (0, self.Presentation.Slides.Count - 1):
+	        time.sleep(2)
+	        self.SS.View.Next()
+	    os.system("taskkill /im powerpnt.exe /f")
+
+
+	def show_slide(self, num):
+		SlideShowWindows(1).View.GotoSlide(num)
+
+	def restart(self):
+		self.Application = win32com.client.Dispatch("PowerPoint.Application")

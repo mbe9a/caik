19df3851e5be7fe310f52b162e2af74a3221e610
Noah Sauber <nds5yf@virginia.edu>
2016-06-01 09:50:59 -0400

    gui

--- caik/gui.py
+++ caik/gui.py
@@ -7,29 +7,33 @@ UVA THZ CAI
 
 from Tkinter import *
 
-#setup window
+#initialize window
 root = Tk()
 root.title("Teraherts Coded Aperture Imaging")
 root.geometry("500x500")
 
+#labels
 label = Label(root, text = "Select a rank ")
 label1 = Label(root, text = "Select an imaging technique ")
 
-rank = Entry(root)
+#control variables
 x = StringVar()
 
+#widgets
 method = OptionMenu(root, x, "Hadamard", "Raster", "Bar")
 run_button = Button(root, text = "Run")
+rank = Entry(root)
 
-label.grid(row = 0)
-label1.grid(row = 1)
-
+#layout
+label.grid(row = 0, sticky = E)
+label1.grid(row = 1, sticky = E)
 rank.grid(row = 0, column = 1)
 method.grid(row = 1, column = 1)
-
-
 run_button.grid(row = 5)
 
 
+def run():
+	root.mainloop()
 
-root.mainloop()
+def exit():
+	root.quit()

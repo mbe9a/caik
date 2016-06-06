09bc282c2fc8dac0524486abb63000288a3b9080
Noah Sauber <nds5yf@virginia.edu>
2016-06-06 16:45:37 -0400

    more gui

--- caik/gui.py
+++ caik/gui.py
@@ -59,6 +59,11 @@ class GUI(Tk):
 		#menu
 		self.menubar = Menu(self)
 		self.filemenu = Menu(self.menubar, tearoff = 0)
+		self.filemenu.add_command(label = 'New File')
+		self.filemenu.add_command(label = 'Open File')
+		self.filemenu.add_command(label = 'Save')
+		self.filemenu.add_command(label = 'Save as...')
+		self.filemenu.add_separator()
 		self.filemenu.add_command(label = 'Open Cal...', command = self.open_cal)
 		self.filemenu.add_command(label = 'Save Cal...', command = self.save_cal)
 		self.filemenu.add_separator()
@@ -66,7 +71,7 @@ class GUI(Tk):
 	
 		self.config(menu=self.menubar)
 
-		self.show_frame('Home')
+		self.show_frame('Cap')
 
 		
 	def open_cal(self):
@@ -76,6 +81,7 @@ class GUI(Tk):
 			return
 		self.cal = rf.read(f)
 		self.status.config(text = 'Calibration loaded successfully')
+
 
 	def save_cal(self):
 		f = tkFileDialog.asksaveasfile(mode = 'w', defaultextension = '.cal')
@@ -215,64 +221,192 @@ class Cap(Frame):
 
 		Frame.__init__(self, parent)
 		self.controller = controller
+		self.params = Frame(self, bg = 'blue')
 
-		#control variables
-		self.method_c = StringVar()
-		self.var_c = IntVar()
+		#hadamard interface
 		self.scale_c = IntVar()
-		self.xlen_c = IntVar()
-		self.ylen_c = IntVar()
+		self.scale_l = Label(self.params, text = "Scale")
+		self.scale = Entry(self.params, textvariable = self.scale_c)
+
 		self.rank_c = IntVar()
+		self.rank_l = Label(self.params, text = 'Rank')
+		self.rank = Entry(self.params, textvariable = self.rank_c)
 
+		self.take_c = BooleanVar()
+		self.take_c.set(True)
+		self.take = Checkbutton(self.params, text = 'Take', variable = self.take_c)
 
-		#buttons
-		Button(self, text = 'Take Image', command = self.image).pack()
+ 		self.mask_delay_c = IntVar()
+ 		self.mask_delay_c.set(1)
+		self.mask_delay_l = Label(self.params, text = 'Delay between masks')
+		self.mask_delay = Entry(self.params, textvariable = self.mask_delay_c)
 
-		Radiobutton(self, text = 'Primary', variable = self.var_c, value = 0).pack()
-		Radiobutton(self, text = 'Inverted', variable = self.var_c, value = 1).pack()
-		Radiobutton(self, text = 'Both', variable = self.var_c, value = 2).pack()
+ 		self.duty_c = DoubleVar()#Not implemented
+ 		
+ 		self.num_meas_c = IntVar()
+ 		self.num_meas_c.set(1)
+ 		self.num_meas_l = Label(self.params, text = 'Number of measurements per mask')
+		self.num_meas = Entry(self.params, textvariable = self.num_meas_c)
 
-		#options
-		self.om = OptionMenu(self, self.method_c, 'hadamard', 'raster', 'bar')
+ 		self.avg_delay_c = IntVar()
+ 		self.avg_delay_c.set(0)
+ 		self.avg_delay_l = Label(self.params, text = 'Delay between measurements')
+		self.avg_delay = Entry(self.params, textvariable = self.avg_delay_c)
+
+ 		self.apply_cal_c = BooleanVar()
+ 		self.apply_cal_c.set(True)
+ 		self.apply_cal = Checkbutton(self.params, text = 'Apply calibration', variable = self.apply_cal_c)
+
+ 		self.averaging_c = BooleanVar()
+ 		self.averaging_c.set(True)
+		self.averaging = Checkbutton(self.params, text = 'Average', variable = self.averaging_c) 		
+
+ 		self.caching_c = BooleanVar()
+ 		self.caching_c.set(True)
+ 		self.caching = Checkbutton(self.params, text = 'Cache', variable = self.caching_c)
+
+ 		self.viewf_c = IntVar()
+ 		self.viewf_c.set(634)
+ 		self.viewf_l = Label(self.params, text = 'View frequency')
+		self.viewf = Entry(self.params, textvariable = self.viewf_c)
+
+		self.attr_c = StringVar()
+		self.attr_c.set('s_db')
+		self.attr_l = Label(self.params, text = 'Attribute')
+		self.attr = Entry(self.params, textvariable = self.attr_c)
+		
+		#raster
+		self.xlen_c = IntVar()
+		self.xlen_l = Label(self.params, text = 'X Length')
+		self.xlen = Entry(self.params, textvariable = self.xlen_c)
+
+		self.ylen_c = IntVar()
+		self.ylen_l = Label(self.params, text = 'Y Length')
+		self.ylen = Entry(self.params, textvariable = self.ylen_c)
+
+		#bar
+		self.parts_c = IntVar()
+
+		#all methods
+		self.top_frame = Frame(self, bg = 'green')
+		self.method_c = StringVar()
 		self.method_c.trace('w', self.update)
-		self.om.pack()
+		self.method_c.set('hadamard')
+		Label(self.top_frame, text = 'Imaging method').pack(side = LEFT, padx = 5, pady = 5)
+		OptionMenu(self.top_frame, self.method_c, 'hadamard', 'raster', 'bar').pack(side = LEFT, padx = 5, pady = 5)
+		self.top_frame.pack(side = TOP, fill = X)
 
-		#entries
-		self.scale = Entry(self, textvariable = self.scale_c)
-		self.rank = Entry(self, textvariable = self.rank_c)
-		self.xlen = Entry(self, textvariable = self.xlen_c)
-		self.ylen = Entry(self, textvariable = self.ylen_c)
 
-		#labels
-		self.l_rank = Label(self, text = 'Select rank ')
-		self.l_method = Label(self, text = 'Select an imaging method ')
+		self.params.pack(side = TOP, fill = 'both', expand = True)
+
+		self.bot_frame = Frame(self, bg = 'yellow')
+		
+		self.name_c = StringVar()
+		Label(self.bot_frame, text = 'Name').pack(side = LEFT, padx = 5, pady = 5)
+		Entry(self.bot_frame, textvariable = self.name_c).pack(side = LEFT, pady = 5)
+
+		self.var_c = IntVar()
+		self.var_c.set('primary')
+		#Label(self.bot_frame, text = 'Mask variation').pack(side = LEFT)
+		OptionMenu(self.bot_frame, self.var_c, 'primary', 'inverse', 'both').pack(side = LEFT, padx = 5, pady = 5)
+		
+		Button(self, bg = 'yellow', text = 'Take Image', command = self.image).pack(side = RIGHT, padx = 5, pady = 5)
+		self.bot_frame.pack(side = BOTTOM, fill = X)
+
 
 	def update(self, n, m, x):
 		f = self.method_c.get()
 
 		if f == 'hadamard':
-			self.rank.pack()
-			self.scale.pack()
-			self.xpos.pack_forget()
-			self.ypos.pack_forget()
+			self.clear_params()
+			self.pack_hadamard()
+			return
 
 		if f == 'raster':
-			self.rank.pack_forget()
-			self.scale.pack()
-			self.xlen.pack()
-			self.ylen.pack()
-		
+			self.clear_params()
+			self.pack_raster()
+			return
+
 		if f == 'bar':
-			self.rank.pack()
-			self.scale.pack()
-			self.xlen.pack_forget()
-			self.ylen.pack_forget()
+			self.clear_params()
+			self.pack_bar()
+			return
 
 	def image(self):
-		f = self.var.get()
+		f = self.var_c.get()
+		
 		if f is 'hadamard':
-			obj = pro.F()
-		self.controller.cai.take_hadamard_image(obj)
+			self.controller.kind = pro.hadamard(rank = self.rank_c.get(), scale = self.scale_c.get(), varient = self.var_c.get())
+			self.controller.cai.hadamard_image(self.controller.kind)
+
+		if f is 'raster':
+			self.controller.kind = pro.raster()
+
+		if f is 'bar':
+			self.controller.kind = pro.bar()
+
+	def clear_params(self):
+		self.scale.grid_forget()
+		self.scale_l.grid_forget()
+		self.rank.grid_forget()
+		self.rank_l.grid_forget()
+		self.take.grid_forget()
+		self.mask_delay.grid_forget()
+		self.mask_delay_l.grid_forget()
+		#self.duty.grid_forget()
+		self.num_meas.grid_forget()
+		self.num_meas_l.grid_forget()
+		self.avg_delay.grid_forget()
+		self.avg_delay_l.grid_forget()
+		self.apply_cal.grid_forget()
+		self.averaging.grid_forget()
+		self.caching.grid_forget()
+		self.viewf.grid_forget()
+		self.viewf_l.grid_forget()
+		self.attr.grid_forget()
+		self.attr_l.grid_forget()
+		self.xlen.grid_forget()
+		self.xlen_l.grid_forget()
+		self.ylen.grid_forget()
+		self.ylen_l.grid_forget()
+
+	def pack_hadamard(self):
+
+		self.scale_l.grid(row = 0, column = 0, padx = 2, pady = 2, sticky = E)
+		self.scale.grid(row = 0, column = 1, padx = 2, pady = 2)
+		
+		self.rank_l.grid(row = 1, column = 0, sticky = E, padx = 2, pady = 2)
+		self.rank.grid(row = 1, column = 1, padx = 2, pady = 2)
+		
+		self.mask_delay_l.grid(row = 2, column = 0, sticky = E, padx = 2, pady = 2)
+		self.mask_delay.grid(row = 2, column = 1, padx = 2, pady = 2)
+	
+		self.num_meas_l.grid(row = 3, column = 0, sticky = E, padx = 2, pady = 2)
+		self.num_meas.grid(row = 3, column = 1, padx = 2, pady = 2)
+		
+		self.avg_delay_l.grid(row = 4, column = 0, sticky = E, padx = 2, pady = 2)
+		self.avg_delay.grid(row = 4, column = 1, padx = 2, pady = 2)
+
+		# self.viewf_l.grid(row = 5, column = 0, sticky = E, padx = 2, pady = 2)
+		# self.viewf.grid(row = 5, column = 1, padx = 2, pady = 2)
+
+		# self.attr_l.grid(row = 6, column = 0, sticky = E, padx = 2, pady = 2)
+		# self.attr.grid(row = 6, column = 1, padx = 2, pady = 2)
+
+		#self.duty.grid()
+		
+		#self.apply_cal.grid(row = 0, column = 2, sticky = W, padx = 2, pady = 2)
+		#self.averaging.grid(row = 1, column = 2, sticky = W, padx = 2, pady = 2)
+		#self.caching.grid(row = 2, column = 2, sticky = W, padx = 2, pady = 2)
+		#self.take.grid(row = 3, column = 2, sticky = W, padx = 2, pady = 2)
+		
+
+	def pack_raster(self):
+		return 0
+
+	def pack_bar(self):
+		return 0
+
 
 class View(Frame):
 	def __init__(self, parent, controller):
@@ -283,6 +417,6 @@ class View(Frame):
 
 root = GUI()
 root.title('Terahertz Coded Aperture Imaging')
-root.geometry('500x400')
+root.geometry('600x500')
 root.resizable(0,0)
 root.mainloop()
